f78910429bcbc0890e38319ce563f32f4dc7274e
DylanNguyen8 <dylan.nguyen1@stu.ocsb.ca>
2024-10-31 12:24:20 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+//PART ONE: START STORY
+function startGame() {
+  let name = prompt("wElCoMe fRieNd,tO tHe hAuNtEd ALL SAINTS HIGHSCHOOL, wHere iT mAy seEm lIkE a nOrMal hIgHsChOoL, bUt wHeN iT tUrNs dArk, tHiNgs gGt rEal SPOOKY. WhAt iS yOur nAme, FRIEND?")
+      alert("WeLCoMe " + name + "," " let's eXpLore tHiS HAunTed sCHooooooL.");
+}
