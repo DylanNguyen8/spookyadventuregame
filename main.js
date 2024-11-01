b64aa95b5e665271554a9e7a2e534c3431aa408e
DylanNguyen8 <dylan.nguyen1@stu.ocsb.ca>
2024-11-01 16:58:12 +0000

    FINAL

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+let choice1;
+let choice2;
+let choice3;
+
 function main() {
       startGame();
-      decision1();
-      decision2();
+      decision1and2();
 
 }
 
@@ -14,24 +17,33 @@ function startGame() {
 
 
 //PART TWO: DECISION 1
-function decision1(){
-let choice1 = prompt("YOu eNter the maIn eNtraNce of The sChool. Do yOu exPlore the first floOr, or the seCond flOor?");
-      alert("Your chose to explore " + choice1 + "... gOod cHoIce.");
-
-}
+function decision1and2(){
+choice1 = prompt("YOu eNter the maIn eNtraNce of The sChool. Do yOu exPlore the library, or the gym?");
+      alert("Your chose to explore the " + choice1 + "... gOod cHoIce.");
 
 //PART THREE: DECISION 2
-function decision2(){
-      let choice2 = prompt("Now, yOu aRe oN tHe " + choice1 + ", yOu hEaR a vOice cAlling yoUr name. inVestigAte? oR nOt?");
-      alert("Your chose to " + choice2);
-      
+ choice2 = prompt("Now, yOu aRe iN thE " + choice1 + ", yOu hEaR a vOice cAlling yoUr name tO the sTorage roOm. inVestigAte? YES or NO?");  
+      if (choice2 == "YES"){
+            alert("YOu cHose tO inVestiGate The SToRage rOom. yOu mIght rEgret tHat...");
+//PART FOUR DECISION 3
+choice3 = prompt("Now, yOu aRe iN thE sTorage roOm. YOu seE a litTle gIrl cRying iN the coRner, Do you: COMFORT oR RUN?");
+      if (choice3 == "COMFORT"){
+      alert("iT was A ghost tHaT feEls loneLy, Comforting tHe gHost made Her feeL happy! She Disappears aNd yOu are Rewarded wiTh CANDY! THE END!");
+
+      }
 }
+      if (choice2 == "NO") {
+            alert("YOu cHose tO inVestiGate The SToRage rOom. aRe yOu ScaRed???")
+
+choice3 = prompt("Now, yOu aRe iN thE sTorage roOm. YOu seE a litTle gIrl cRying iN the coRner, Do you: COMFORT oR RUN?");
+if (choice3 == "RUN"){
+alert("iT was A ghost tHaT feEls loneLy, RunNing aWay made Her feeL ANGRY! She cHaseS yOu yOu are Rewarded wiTh... DEATH! THE END!");
+      
+      }
+
+
+}
+}
+          
 
 main();
-
-
-
-
-
-
-
